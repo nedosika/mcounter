4b9dc8d066b8c6d56da0d8633e1d3a3d28bcc0c6
nedosika <nedosika@gmail.com>
2021-01-20 11:57:44 +0200

    add check negative values

--- src/actions/decrement.js
+++ src/actions/decrement.js
@@ -2,11 +2,12 @@ import {UPDATE_PLAYER} from "../store";
 
 export const decrement = (id, field, players) => {
     const index = players.findIndex(item => item.id === id);
+    const newValue = players[index][field] > 0 ? players[index][field] - 1 : 0;
     return {
         type: UPDATE_PLAYER,
         payload: {
             id,
-            [field]: players[index][field] - 1
+            [field]: newValue
         }
     }
 }
